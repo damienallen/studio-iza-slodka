b3b712d865c7da1e846c2b162eca7810bad34d1d
Damien Allen <contact@dallen.co>
2019-12-14 16:02:41 +0100

    Reorganized scss, improved layout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import 'normalize.css'
-import './styles/breakpoints.scss';
+import './styles/constants.scss';
 import './styles/theme.scss';
 import './styles/layout.scss';
+import './styles/sidebar.scss';
+import './styles/nav.scss';
 import './styles/content.scss';
 
 console.log('Initializing JS...')

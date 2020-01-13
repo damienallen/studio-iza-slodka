90a453c07f2c7bf304895fe5aaf9bfdb145e2a6f
Damien Allen <contact@dallen.co>
2020-01-13 01:25:42 +0100

    Added diagrams

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ImageminPlugin = require('imagemin-webpack');
+// const ImageminPlugin = require('imagemin-webpack');
 const path = require('path');
 
 module.exports = {
@@ -28,31 +28,31 @@ module.exports = {
                 to: 'ico'
             }
         ]),
-        new ImageminPlugin({
-            bail: false, // Ignore errors on corrupted images
-            cache: true,
-            imageminOptions: {
-              // Before using imagemin plugins make sure you have added them in `package.json` (`devDependencies`) and installed them
+        // new ImageminPlugin({
+        //     bail: false, // Ignore errors on corrupted images
+        //     cache: true,
+        //     imageminOptions: {
+        //       // Before using imagemin plugins make sure you have added them in `package.json` (`devDependencies`) and installed them
        
-              // Lossless optimization with custom option
-              // Feel free to experiment with options for better result for you
-              plugins: [
-                ["gifsicle", { interlaced: true }],
-                ["jpegtran", { progressive: true }],
-                ["optipng", { optimizationLevel: 5 }],
-                [
-                  "svgo",
-                  {
-                    plugins: [
-                      {
-                        removeViewBox: false
-                      }
-                    ]
-                  }
-                ]
-              ]
-            }
-        })
+        //       // Lossless optimization with custom option
+        //       // Feel free to experiment with options for better result for you
+        //       plugins: [
+        //         ["gifsicle", { interlaced: true }],
+        //         ["jpegtran", { progressive: true }],
+        //         ["optipng", { optimizationLevel: 5 }],
+        //         [
+        //           "svgo",
+        //           {
+        //             plugins: [
+        //               {
+        //                 removeViewBox: false
+        //               }
+        //             ]
+        //           }
+        //         ]
+        //       ]
+        //     }
+        // })
     ],
 
     module: {
@@ -71,14 +71,14 @@ module.exports = {
                 test: /\.(s*)css$/,
                 use: ['style-loader', 'css-loader?url=false', 'sass-loader']
             },
-            {
-            test: /\.(jpe?g|png|gif|svg)$/i,
-            use: [
-                {
-                loader: 'file-loader' // Or `url-loader` or your other loader
-                }
-            ]
-            }
+            // {
+            // test: /\.(jpe?g|png|gif|svg)$/i,
+            // use: [
+            //     {
+            //     loader: 'file-loader' // Or `url-loader` or your other loader
+            //     }
+            // ]
+            // }
         ]
     },
 

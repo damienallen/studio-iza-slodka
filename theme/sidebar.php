98c1894801500d9d9fafb01d0e49e7f867a8e23a
Damien Allen <contact@dallen.co>
2021-05-02 15:59:23 +0000

    Removed up arrow, final caddy config

--- theme/sidebar.php
+++ theme/sidebar.php
@@ -2,22 +2,7 @@
 
     <div class="logo-container">
         <img id="logo" src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png" alt="" />
-        <div id="top-arrow">
-            <svg
-                xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-                xmlns:svg="http://www.w3.org/2000/svg"
-                xmlns="http://www.w3.org/2000/svg"
-                version="1.1"
-                x="0px"
-                y="0px"
-                width="120"
-                height="150"
-                viewBox="0 0 512 512"
-            >
-                <path d="M 256,56.094248 464,575.99152 h 48 L 256,-63.882047 0,575.99152 h 48 z"
-                    style="stroke-width:1.58098257" />
-            </svg>
-        </div>
+        <div id="top-arrow"></div>
     </div>
 
     <a href="<?php echo get_bloginfo( 'wpurl' );?>">

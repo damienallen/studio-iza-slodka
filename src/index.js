fbee3c104c25e7bcb156236ae8e4ed83c08af0fe
Damien Allen <contact@dallen.co>
2022-03-28 23:34:57 +0200

    Updated bundles

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ import './styles/sidebar.scss'
 import './styles/nav.scss'
 import './styles/news.scss'
 import './styles/content.scss'
-
-import $ from 'jquery'
 import './scripts/mobile_toggles.js'
 import './scripts/navigation.js'
 import './scripts/sidebar.js'
 import './scripts/jScrollability.js'
 
+import $ from 'jquery'
+
 $(() => {
     $('.front-cover').css({ height: window.innerHeight })
 
     const now = new Date()
-    console.log(`Loaded at ${now.toLocaleTimeString()} on ${now.toLocaleDateString()}`)
+    console.log(`> Loaded at ${now.toLocaleTimeString()} on ${now.toLocaleDateString()}`)
 })

1496e411eb7879aaf8063ae33890aace81d29d5b
Damien Allen <contact@dallen.co>
2022-03-29 23:45:19 +0200

    Smooth scroller

--- src/scripts/scroller.js
+++ src/scripts/scroller.js
@@ -1,15 +1,35 @@
 // Section scroller
-const frontFactor = 1
-const middleFactor = 0.5
-const backFactor = 0.2
-const spyOffset = 100
+const spyOffset = 100 // px
+const duration = 50 // ms
+const throttle = 50 // ms
 
-const duration = 50
-const throttle = 50
+const scrollSettings = {
+    text: {
+        start: 0,
+        end: 1.1,
+        direction: -1
+    },
+    front: {
+        start: 0,
+        end: 1.1,
+        direction: -1
+    },
+    middle: {
+        start: 0,
+        end: 1,
+        direction: 1
+    },
+    back: {
+        start: 0.1,
+        end: 2,
+        direction: -1
+    },
+}
 
 const scrollSection = (scrollTop) => {
     const mazeSection = $('#maze-section')
     const sectionOffset = mazeSection.offset()
+
     if (sectionOffset) {
         const elementTop = sectionOffset.top
         const elementHeight = mazeSection.height()
@@ -18,18 +38,17 @@ const scrollSection = (scrollTop) => {
         const scrollBottom = scrollTop + window.innerHeight
 
         if (scrollBottom > elementTop + spyOffset && scrollTop < elementBottom - spyOffset) {
-
-
-            const progress = 1 - (elementHeight - Math.min(scrollTop, elementHeight)) / elementHeight
+            const progress =
+                1 - (elementHeight - Math.min(scrollTop, elementHeight)) / elementHeight
             const elementWidth = $('#maze-section>.spacer').width()
-            const leftOffset = progress * (window.innerWidth - elementWidth)
+            console.log(elementWidth, '>>', progress)
 
-            console.log(Math.min(scrollTop, elementHeight), elementHeight, '>>', progress)
+            for (const [subclass, { start, end, direction }] of Object.entries(scrollSettings)) {
+                const distance = end - start
+                const leftOffset = ((start + distance * progress) * direction * elementWidth) / 3
 
-            $('.scroll-text').animate({left: leftOffset * frontFactor}, duration)
-            $('.scroll-front').animate({left: leftOffset * frontFactor}, duration)
-            $('.scroll-middle').animate({left: leftOffset * middleFactor}, duration)
-            $('.scroll-back').animate({left: leftOffset * backFactor}, duration)
+                $(`.scroll-${subclass}`).animate({ left: leftOffset }, duration)
+            }
         }
     }
 }
@@ -38,10 +57,10 @@ scrollSection(0)
 
 let time = Date.now()
 $(window).scroll((e) => {
-    if ((time + throttle - Date.now()) < 0) {
+    if (time + throttle - Date.now() < 0) {
         const scrollTop = $(e.currentTarget).scrollTop()
         scrollSection(scrollTop)
 
         time = Date.now()
-      }
+    }
 })
